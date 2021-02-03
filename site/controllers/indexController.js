6b6965ba7d826cafe89e1f2daca1d137c2f95223
MichelVarela <michelvarela.95@outlook.com>
2021-02-03 17:58:10 -0300

    cree ejs,rutas y css

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,8 +1,14 @@
 module.exports={
     index:(req,res)=>{
-        res.render('index',{
-            title : "MascoShop"
-        });
+        res.render('index');
+        
+    },
+    carrito:(req,res)=>{
+        res.render('productCart');
+        
+    },
+    product:(req,res)=>{
+        res.render('productAdd');
         
     }
 }
--- site/routes/index.js
+++ site/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {index}=require('../controllers/indexController');
+const {index,carrito,product}=require('../controllers/indexController');
+
+
 /* GET home page. */
 router.get('/',index);
+router.get('/productCart',carrito);
+router.get('/productAdd',product)
+
 module.exports = router;
