f17dee91d27fe23d38da642e25870677a7417753
MichelVarela <michelvarela.95@outlook.com>
2021-02-15 14:19:29 -0300

    trabajando en rutas product y crud

--- site/controllers/productsControllers.js
+++ site/controllers/productsControllers.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const data = require('../data/dataproducts');
+
 module.exports = {
     
     productCart:(req,res)=>{
@@ -8,8 +11,69 @@ module.exports = {
         res.render('productAdd');
         
     },
+    processProduct:(req,res)=>{
+        const {category,subcategory,producto,precio,stock,state,description} = req.body;
+
+        let lastId = 0;
+
+        data.forEach(element=>{
+            if(element.id > lastId){
+                return lastId = element.id;
+            }
+        });
+
+        const newProduct = {
+            id: +lastId + 1,
+            category: category,
+            subcategory: subcategory,
+            producto: producto,
+            precio: precio,
+            stock: stock,
+            state: state,
+            description: description,
+            img: req.files[0].filename
+        }
+
+        data.push(newProduct);
+        fs.writeFileSync('./data/products.json',JSON.stringify(data,null,2),'utf-8');
+
+        res.redirect('/products/productAdd');
+
+    },
     productDetail:(req,res)=>{
-        res.render('productDetail');
+        let product = data.find(element => element.id === +req.params.id);
+        
+        res.render('productDetail',{product});
+    },
+    productEdit:(req,res)=>{
+        let product = data.find(element => element.id === +req.params.id);
+
+        res.render('productEdit',{product});
+    },
+    processEdit:(req,res)=>{
+        const {category,subcategory,producto,precio,stock,state,description} = req.body;
+
+        data.forEach(element=>{
+            if(element.id == req.params.id){
+                element.id = element.id;
+                element.category = category;
+                element.subcategory = subcategory;
+                element.producto = producto;
+                element.precio = precio;
+                element.stock = stock;
+                element.state = state;
+                element.description = description;
+                element.img = element.img;
+            }
+
+        });
+
+        fs.writeFileSync('./data/products.json',JSON.stringify(data,null,2),'utf-8');
+
+        res.redirect('/products/productAdd');
+    },
+    allProducts:(req,res)=>{
+        res.render('allProducts');
     }
 
 }
--- site/routes/productsRouter.js
+++ site/routes/productsRouter.js
@@ -1,10 +1,28 @@
-const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
-const {productCart,productDetail,productAdd} = require('../controllers/productsControllers');
+const {productCart,productDetail,productAdd,processProduct,productEdit,processEdit,productDelete,allProducts} = require('../controllers/productsControllers');
+
+/* middlewares */
+const upload = require('../middlewares/multer');
 
 router.get('/productCart',productCart);
-router.get('/productDetail',productDetail);
+
+/* product register */
 router.get('/productAdd',productAdd);
+router.post('/productAdd',upload.any(),processProduct);
+/* product register */
+
+router.get('/productDetail/:id',productDetail);/* detalle de productos segun su id */
+
+/* product edit */
+router.get('/productEdit/:id',productEdit);
+router.put('/productEdit/:id',processEdit);
+/* product edit */
+
+/* product delete */
+
+/* product delete */
+
+router.get('/allProducts',allProducts)/* listado de productos */
 
 module.exports = router
