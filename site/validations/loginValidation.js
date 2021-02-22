72b5c30195ee03bef0d059e75bc32b15f133e350
MichelVarela <michelvarela.95@outlook.com>
2021-02-22 16:41:20 -0300

    borre sobrante

--- site/validations/loginValidation.js
+++ site/validations/loginValidation.js
@@ -1,17 +1,12 @@
-const fs=require('fs');
-const {check,body}=require('express-validator')
-const users_db = JSON.parse(fs.readFileSync('./data/users.json','utf-8'))
-const bcrypt=require('bcrypt');
+const {check}=require('express-validator');
 
 module.exports=[ /* esto es una array que va a estar chequeando si todo es correcto */
   
   
   check('email').isEmail().withMessage('El campo Email tiene que ser un email valido'),
-
-
+  
+  
   check('pass')
   .notEmpty().withMessage('La contraseña es requerida'),
-
- 
   
 ]
