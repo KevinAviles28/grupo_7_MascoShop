cd92ed3aebdb058e022d92f72278335819f64d2b
MichelVarela <michelvarela.95@outlook.com>
2021-03-20 11:48:12 -0300

    cambios en la vista

--- site/controllers/productsControllers.js
+++ site/controllers/productsControllers.js
@@ -178,7 +178,7 @@ module.exports = {
                 Promise.all([eliminarImagen, eliminarProducto])
                     .then((result) => {
                         imagen.forEach(element => {
-                            if (element.product_name != 'productoDefoult.png') {
+                            if (element.product_name != 'productoDefault.png') {
                                 fs.unlinkSync('public/images/productos/' + element.product_name)
                             }
                         })
