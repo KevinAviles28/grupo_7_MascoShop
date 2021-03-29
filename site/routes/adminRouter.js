7ac6a2709d81b846c6c1c79a4e41cfb912050765
MichelVarela <michelvarela.95@outlook.com>
2021-03-29 00:44:13 -0300

    cambios

--- site/routes/adminRouter.js
+++ site/routes/adminRouter.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const adminCheck= require(path.join('..','middlewares','admincheck'));
+
 const {lista,edit,remove} = require(path.join('..','controllers','adminController'));
 
-router.get('/lista',lista);
+router.get('/lista',adminCheck,lista);
 router.put('/edit/:id',edit);
 router.delete('/delete/:id',remove);
 
