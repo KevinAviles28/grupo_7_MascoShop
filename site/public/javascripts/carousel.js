9800d3ca68ac66a0ec1847d363e8d973368a5866
MichelVarela <michelvarela.95@outlook.com>
2021-04-02 02:01:31 -0300

    cambio slider

--- site/public/javascripts/carousel.js
+++ site/public/javascripts/carousel.js
@@ -1,73 +1,71 @@
 window.addEventListener('load', function(){
-	new Glider(document.querySelector('.carousel__lista'), {
-		slidesToShow: 1,/* cuantos slide a mostrar en el carrousel */
-		slidesToScroll: 1,/* cantidad de slides que se desplazan */
-		arrows: {
-			prev: '.carousel__anterior',
-			next: '.carousel__siguiente'
+	new Glider(document.querySelector('.glider'), {
+	  slidesToShow: 1,/* numero de diapositivas que se veran */
+	  slidesToScroll: 1,/* numero de diapositivas que se desplazaran, auto toma el mismo valor que slideToShow */
+	  arrows: {/* controlador de flechas, valores prev && next */
+		prev: '.glider-prev',
+		next: '.glider-next'
+	  },
+	  responsive: [/* array de objetos donde declaramos breackpoints,settings */
+		{
+		  // vista >= 575px
+		  breakpoint: 575,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},{
+		  // vista >= 775px
+		  breakpoint: 775,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		  }
 		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 575,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 2,
-				slidesToScroll: 2
-			  }
-			},{
-			  // nuevo point
-			  breakpoint: 775,
-			  settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3
-			  }
-			},
-			{
-				// screens greater than >= 1024px
-				breakpoint: 1024,
-				settings: {
-				  slidesToShow: 4,
-				  slidesToScroll: 4
-				}
-			  }
-		]
-	});
-});
-
-window.addEventListener('load', function(){
-	new Glider(document.querySelector('.carousel__lista1'), {
-		slidesToShow: 1,/* cuantos slide a mostrar en el carrousel */
-		slidesToScroll: 1,/* cantidad de slides que se desplazan */
-		arrows: {
-			prev: '.anterior1',
-			next: '.siguiente1'
+		{
+		  // vista >= 1024px
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 4,
+			slidesToScroll: 4
+		  }
+		}
+	  ]
+	})
+  })
+  
+  window.addEventListener('load', function(){
+	new Glider(document.querySelector('.glider1'), {
+	  slidesToShow: 1,/* numero de diapositivas que se veran */
+	  slidesToScroll: 1,/* numero de diapositivas que se desplazaran, auto toma el mismo valor que slideToShow */
+	  arrows: {/* controlador de flechas, valores prev && next */
+		prev: '.prev1',
+		next: '.next1'
+	  },
+	  responsive: [/* array de objetos donde declaramos breackpoints,settings */
+		{
+		  // vista >= 575px
+		  breakpoint: 575,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},{
+		  // vista >= 775px
+		  breakpoint: 775,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		  }
 		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 575,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 2,
-				slidesToScroll: 2
-			  }
-			},{
-			  // nuevo point
-			  breakpoint: 775,
-			  settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3
-			  }
-			},
-			{
-				// screens greater than >= 1024px
-				breakpoint: 1024,
-				settings: {
-				  slidesToShow: 4,
-				  slidesToScroll: 4
-				}
-			  }
-		]
-	});
-});
+		{
+		  // vista >= 1024px
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 4,
+			slidesToScroll: 4
+		  }
+		}
+	  ]
+	})
+  })
