aeea702b9164a8b4a9e58032505adc74a6735915
MichelVarela <michelvarela.95@outlook.com>
2021-04-02 10:30:08 -0300

    acomodamos ojo

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const db = require(path.join('..','database','models'));
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-const {Op}=require('sequelize')
+const {Op}=require('sequelize');
+const {validationResult} = require('express-validator');
+
 module.exports={
     index:(req,res)=>{
         
@@ -40,19 +42,20 @@ module.exports={
     },
     search:(req,res)=>{
 
-        db.Productos.findAll({
-            include:[{association:"imagenProducto"},{association:"categoria"},{association:"subcategoria"}]
-        })
-        .then(result=>{
-            const search = result.filter(element=>{
-                if(element.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) || element.subcategoria.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) || element.categoria.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) ){
-                    return element;
-                }
+            db.Productos.findAll({
+                include:[{association:"imagenProducto"},{association:"categoria"},{association:"subcategoria"}]
             })
+            .then(result=>{
+                const search = result.filter(element=>{
+                    if(element.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) || element.subcategoria.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) || element.categoria.name.toLowerCase().includes(req.query.busqueda.toLowerCase().trim()) ){
+                        return element;
+                    }
 
-            res.render('search',{search,toThousand});
-        })
-        .catch(error => res.send(error));
+                })
+    
+                res.render('search',{search,toThousand});
+            })
+            .catch(error => res.send(error));
         
     },
     nosotros:(req,res)=>{
--- site/routes/indexRouter.js
+++ site/routes/indexRouter.js
@@ -3,9 +3,10 @@ var router = express.Router();
 const path = require('path');
 
 const {index,search, nosotros,mediosDePago,sucursales} = require(path.join('..','controllers','indexController'));
+const searchValidation = require(path.join('..','validations','searchValidation'));
 
 router.get('/',index);
-router.get('/search',search);
+router.get('/search',searchValidation,search);
 router.get('/sobreNosotros',nosotros);
 router.get('/mediosDePago',mediosDePago);
 router.get('/sucursales',sucursales);
