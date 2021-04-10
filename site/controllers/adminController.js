c024d950bb489b5887762cb26e2454850b6b2dc0
MichelVarela <michelvarela.95@outlook.com>
2021-04-10 14:16:28 -0300

    nueva lista de productos

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const db = require(path.join('..','database','models'));
 const { Op } = require('sequelize');
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = {
     lista:(req,res)=>{
@@ -43,5 +44,128 @@ module.exports = {
             return res.redirect('/admin/lista');
         })
         .catch(error => console.log(error))
+    },
+    listaProducts:(req,res)=>{
+
+        db.Productos.findAll({
+            include: [{association: "imagenProducto"},{association: "subcategoria"},{association: "categoria"}]
+        })
+        .then(products=>{
+
+            res.render('admin/listaProducts',{
+                products,
+                toThousand
+            });
+            
+        })
+        .catch(error=> console.log(error));
+    },
+    productEdit: (req, res) => {
+        
+        let pedidoCategoria = db.Categoria.findAll();
+        let pedidoSubCategoria = db.SubCategoria.findAll();
+        let elProducto = db.Productos.findByPk(req.params.id, {
+            include: [{ association: "categoria" }, { association: "subcategoria" }, { association: "imagenProducto" }]
+        })
+        Promise.all([pedidoCategoria, pedidoSubCategoria, elProducto])
+        .then(([categoria, subcategoria, product]) => {
+            res.render('adminProduct/productEdit', {
+                categoria,
+                subcategoria,
+                product
+            })
+        })
+    },
+    processEdit: (req, res) => {
+        
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            let pedidoCategoria = db.Categoria.findAll()
+            let pedidoSubCategoria = db.SubCategoria.findAll()
+            let pedidoProducto = db.Productos.findByPk(req.params.id)
+            Promise.all([pedidoCategoria, pedidoSubCategoria, pedidoProducto])
+            .then(([categoria, subcategoria, product]) => {
+                return res.render('adminProduct/productEdit', {
+                    categoria,
+                    subcategoria,
+                    product,
+                    errores: errores.mapped(), /* convierte el valor del array en el valor de errors */
+                    old: req.body
+                })
+            })
+        } else {
+            const {name, precio, stock, discount, description, category, subcategory, img} = req.body;
+            let elParams=req.params.id;
+            
+            db.ImagenProducto.findAll({
+                where:{
+                    product_id:elParams
+                }
+            })
+            .then((imagen)=>{
+                let actualizacionProducto= db.Productos.update({
+                    name: name.trim(),
+                    precio: precio,
+                    stock: stock,
+                    discount: discount,
+                    description: description.trim(),
+                    category_id: category,
+                    subcategory_id: subcategory
+                }, {
+                    where: {
+                        id: elParams
+                    }
+                })
+                let actualizacionImagenProducto=db.ImagenProducto.update({
+                    product_name: req.files[0].filename
+                    
+                },{
+                    where:{
+                        id:elParams
+                    }
+                })
+                
+                Promise.all([actualizacionProducto,actualizacionImagenProducto])
+                .then((result)=>{
+                    imagen.forEach(element => {
+                        if (element.product_name != 'productoDefault.png') {
+                            fs.unlinkSync('public/images/productos/' + element.product_name)
+                        }
+                    })
+                    res.redirect("/products/allProducts#productos-destacados")
+                })
+            })
+        }
+    },
+    productDelete: (req, res) => {
+
+        db.ImagenProducto.findOne({
+            where: {
+                product_id: req.params.id
+            }
+        })
+        .then((imagen) => {
+
+            if(imagen.product_name != 'usuarioDefault.png'){
+                fs.unlinkSync('public/images/productos/' + imagen.product_name)
+            }
+
+        })
+
+        db.ImagenProducto.destroy({
+            where: {
+                product_id: req.params.id
+            }
+        })
+
+        db.Productos.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(()=>{
+            res.redirect('/admin/listaProducts');
+        })
+        .catch(error=> console.log(error))
     }
 }
--- site/routes/adminRouter.js
+++ site/routes/adminRouter.js
@@ -3,11 +3,24 @@ const express = require('express');
 const router = express.Router();
 
 const adminCheck= require(path.join('..','middlewares','admincheck'));
+const rutasCheck= require(path.join('..','middlewares','rutasCheck'));
+const upload = require(path.join('..','middlewares','multerProduct'));
+const productoValidation=require(path.join('..','validations','productoValidation'));
 
-const {lista,edit,remove} = require(path.join('..','controllers','adminController'));
+const {lista,edit,remove,listaProducts,productEdit,processEdit,productDelete} = require(path.join('..','controllers','adminController'));
 
 router.get('/lista',adminCheck,lista);
 router.put('/edit/:id',edit);
 router.delete('/delete/:id',remove);
+
+router.get('/listaProducts',adminCheck,listaProducts);
+/* product edit */
+router.get('/productEdit/:id',rutasCheck,adminCheck,productEdit);
+router.put('/productEdit/:id',upload.any(),productoValidation,processEdit);
+/* product edit */
+
+/* product delete */
+router.delete('/productDelete/:id',productDelete);
+/* product delete */
 
 module.exports = router;
